4c036c3ef1873621358704cc989cee8a8be396ee
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-04-22 10:59:56 +0300

    JSDocs for urils

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,27 @@
+/**
+ * Ensure that passed value is object
+ * @param  {*}       object Value to check
+ * @return {boolean}        Check result
+ */
 export function isObject (object) {
   return Object(object) === object
 }
 
+/**
+ * Ensure that passed value could be transfer
+ * @param  {*}       object Value to check
+ * @return {boolean}        Check result
+ */
 export function isTransferable (object) {
   return object instanceof ArrayBuffer
 }
 
+/**
+ * Recursively peek transferables from passed data
+ * @param  {*}             data        Data source
+ * @param  {Array}         [result=[]] Dist array
+ * @return {ArrayBuffer[]}             List of transferables objects
+ */
 export function peekTransferables (data, result = []) {
   if (isTransferable(data)) {
     result.push(data)
@@ -17,6 +33,9 @@ export function peekTransferables (data, result = []) {
   return result
 }
 
+/**
+ * @return {string} Uniq uid
+ */
 export function uuid () {
   return Math.floor((1 + Math.random()) * 1e10).toString(16)
 }
